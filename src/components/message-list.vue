e9ec03ecd97637424b56e3cced8f66ec4e94b398
Remco <remco@voorhoede.nl>
2018-03-06 13:16:55 +0100

    feat: exercise 3

--- src/components/message-list.vue
+++ src/components/message-list.vue
@@ -1,12 +1,17 @@
 
 <template>
   <div class="message-list">
-    <div class="message-list__empty">
+    <div class="message-list__empty" v-if="messages.length === 0">
       No messages
     </div>
     
-    <ol class="message-list__list">
-      <message></message>
+    <ol class="message-list__list" v-else>
+      <message v-for="message of messages"
+        v-bind:key="message.id"
+        v-bind:text="message.text" 
+        v-bind:user="message.user" 
+        v-bind:date="message.date"
+      ></message>
     </ol>
   </div>
 </template>
