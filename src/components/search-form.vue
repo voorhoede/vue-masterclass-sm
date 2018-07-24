4d4ebfae8c66df70dcdca7f400b6f137c9b9b9f2
Remco <remco@voorhoede.nl>
2018-07-24 17:17:45 +0200

    refactor: vue-loader

--- src/components/search-form.vue
+++ src/components/search-form.vue
@@ -1,12 +1,16 @@
 
 <template>
   <form class="search-form">
-    <input type="search" placeholder="Search" class="reset-text search-field">
+    <input type="search" placeholder="Search" v-model="searchText" class="reset-text search-field">
   </form>
 </template>
 
 <script>
 export default {
-  components: {}
+  components: {},
+
+  data() {
+    return window.chat;
+  },
 }
 </script>
--- src/components/search-sidebar.vue
+++ src/components/search-sidebar.vue
@@ -3,7 +3,7 @@
   <aside class="search-sidebar">
     <button class="reset-button search-sidebar__close" aria-label="Close" title="Close"></button>
     <h2 class="search-sidebar__header">0 Messages</h2>
-    <!--<message-list></message-list>-->
+    <message-list :messages="filteredMessages"></message-list>
   </aside>
 </template>
 
@@ -13,6 +13,19 @@ import MessageList from "./message-list"
 export default {
   components: {
     MessageList
+  },
+
+  data() {
+    return window.chat;
+  },
+
+  computed: {
+    // implement your search functionality here as a computed property
+    // use this.searchText as the search query. Search in this.messages
+
+    filteredMessages() {
+      return [];
+    }
   }
 }
 </script>
