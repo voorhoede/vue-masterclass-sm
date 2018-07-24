d2d2ef6f0a459bd95a848f13dd7b8d92e336052b
Remco <remco@voorhoede.nl>
2018-07-24 16:38:38 +0200

    refactor: vue-loader

--- src/components/message-field.vue
+++ src/components/message-field.vue
@@ -1,26 +1,28 @@
 
 <template>
-  <form class="message-field">
-    <!-- bind an input event to the text input -->
-    <input class="reset-text message-field__input" type="text" :value="text" @input="onChangeText" autofocus>
+  <form class="message-field" @submit.prevent="onSubmit">
+    <input class="reset-text message-field__input" type="text" v-model="text" autofocus>
     <input class="message-field__submit" v-bind:disabled="text.trim() === ''" type="submit">
   </form>
 </template>
 
 <script>
+
 export default {
   components: {},
 
   data() {
     return {
-      text: '' //<-- bind this data property to the value of the text input
+      text: ''
     }
   },
 
   methods: {
-    //create a event handler for the input event and write the event.target.value into the local state
-    onChangeText(e) {
-      this.text = e.target.value;
+    onSubmit() {
+      /*
+        - emit a custom event to the chat-section (with the entered text)
+        - in the chat-section bind to the custom event and use the createMessage method to add the message to the messages array
+      */
     }
   }
 }
