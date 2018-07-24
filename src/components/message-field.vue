3dc45f80ae91e3a4b19243aef51e549ed0d302a4
Remco <remco@voorhoede.nl>
2018-07-24 16:14:29 +0200

    refactor: vue-loader

--- src/components/message-field.vue
+++ src/components/message-field.vue
@@ -1,13 +1,24 @@
 
 <template>
   <form class="message-field">
+    <!-- bind a input event to the text input -->
     <input class="reset-text message-field__input" type="text" value="Message" autofocus>
-    <input class="message-field__submit" type="submit">
+    <input class="message-field__submit" v-bind:disabled="text.trim() === ''" type="submit">
   </form>
 </template>
 
 <script>
 export default {
-  components: {}
+  components: {},
+
+  data() {
+    return {
+      text: '' //<-- bind this data property to the value of the text input
+    }
+  },
+
+  methods: {
+    //create a event handler for the input event and write the event.target.value into the local state
+  }
 }
 </script>
